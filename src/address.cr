89976a987c835b8023e865fc8a359a5f936dce29
/raw PONG _GHMoaCXLT <58883403+q9f@users.noreply.github.com>
2019-12-24 14:32:07 +0100

    implement eip-55 test cases

--- src/address.cr
+++ src/address.cr
@@ -12,11 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# implements the Bitcoin address space
+# implements the bitcoin address space
 module Bitcoin
   # generates a bitcoin address for any public key; compressed and uncompressed
   # version 0x00 = btc mainnet; pass different versions for different networks
-  def self.address_from_public(pub : String, version = "00")
+  def self.address_from_public_key(pub : String, version = "00")
     # ensure uncompressed or compressed public keys with prefix
     if pub.size === 130 || pub.size === 66
       # perform sha-256 hashing on the public key
@@ -25,10 +25,10 @@ module Bitcoin
       # perform ripemd-160 hashing on the result of sha-256
       ripe = Crypto.ripemd160 sha2
 
-      # add version byte in front of ripmd-160 hash
+      # add version byte in front of ripemd-160 hash
       ripe_versioned = "#{version}#{ripe}"
 
-      # perform sha-256 hash on the extended ripmd-160 result
+      # perform sha-256 hash on the extended ripemd-160 result
       # perform sha-256 hash on the result of the previous sha-256 hash
       hashed = Crypto.sha256 ripe_versioned
       hashed_twice = Crypto.sha256 hashed
@@ -48,10 +48,10 @@ module Bitcoin
   end
 
   # generates a bitcoin address from an public key ec point
-  def self.address_from_point(p : Secp256k1::EC_Point, version = "00")
+  def self.address_from_public_point(p : Secp256k1::EC_Point, version = "00")
     # take the corresponding public key generated with it
     pub = Secp256k1.public_key_compressed_prefix p
-    return address_from_public pub, version
+    return address_from_public_key pub, version
   end
 
   # generates a bitcoin address from a private key
@@ -60,7 +60,7 @@ module Bitcoin
     # take the corresponding public key generated with it
     priv = BigInt.new priv, 16
     p = Secp256k1.public_key_from_private priv
-    return address_from_point p, version
+    return address_from_public_point p, version
   end
 end
 
@@ -71,28 +71,38 @@ module Ethereum
     # make sure the address is downcase
     adr = adr.downcase
 
-    # get a keccak-256 to operate on according to eip-55
-    keccak = Crypto.keccak256_string adr
-
-    # prefix the address with `0x`
-    address = "0x"
-
-    # iterate each character to determine capitalization
-    i = 0
-    while i < adr.size
-      k = keccak[i].to_i 16
-      if k >= 8
-        address += "#{adr[i]}".upcase
-      else
-        address += "#{adr[i]}"
-      end
-      i += 1
+    if adr.size === 42
+      # trim a leading `0x`
+      adr = adr[2, 40]
     end
-    return address
+
+    if adr.size === 40
+      # get a keccak-256 to operate on according to eip-55
+      keccak = Crypto.keccak256_string adr
+
+      # prefix the address with `0x`
+      address = "0x"
+
+      # iterate each character to determine capitalization
+      i = 0
+      while i < adr.size
+        k = keccak[i].to_i 16
+        if k >= 8
+          address += "#{adr[i]}".upcase
+        else
+          address += "#{adr[i]}"
+        end
+        i += 1
+      end
+      return address
+    else
+      raise "malformed ethereum address (invalid size: #{adr.size})"
+    end
+    return "-999"
   end
 
   # generates an ethereum address for an uncompressed public key
-  def self.address_from_public(pub : String)
+  def self.address_from_public_key(pub : String)
     # ensure uncompressed public keys
     if pub.size === 128
       # hashes the uncompressed public key with keccak-256
@@ -107,10 +117,10 @@ module Ethereum
   end
 
   # generates an ethereum address from an public key ec point
-  def self.address_from_point(p : Secp256k1::EC_Point)
+  def self.address_from_public_point(p : Secp256k1::EC_Point)
     # take the corresponding public key generated with it
     pub = Secp256k1.public_key_uncompressed p
-    return address_from_public pub
+    return address_from_public_key pub
   end
 
   # generates an ethereum address from a private key
@@ -119,6 +129,6 @@ module Ethereum
     # take the corresponding public key generated with it
     priv = BigInt.new priv, 16
     p = Secp256k1.public_key_from_private priv
-    return address_from_point p
+    return address_from_public_point p
   end
 end
