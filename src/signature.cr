9c778e4de4d296edc8d07b9477de07524f0f84f6
/raw PONG _GHMoaCXLT <58883403+q9f@users.noreply.github.com>
2020-01-07 21:46:06 +0100

    improve documentation for the signature module

--- src/signature.cr
+++ src/signature.cr
@@ -12,50 +12,93 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# implements ecdsa signature generation and verification for secp256k1
-# ref: https://cryptobook.nakov.com/digital-signatures/ecdsa-sign-verify-messages
+# Implements `ECDSA_Signature` generation and verification for `Secp256k1`
+# elliptic curves.
+# Ref: [cryptobook.nakov.com/digital-signatures/ecdsa-sign-verify-messages](https://cryptobook.nakov.com/digital-signatures/ecdsa-sign-verify-messages)
 module Secp256k1::Signature
-  # the ecdsa signing algorithm (rfc 6979) takes as input a message `msg`
+  # Signs a message and creates a signature proof using a private key.
+  #
+  # The ECDSA signing algorithm (RFC-6979) takes as input a message `msg`
   # and a private key `priv`. It produces as output a signature, which
-  # consists of pair of integers `(r, s)`.
+  # consists of pair of integers `(r, s)`, where `r` is the `x`-coordinate
+  # of a random point on our curve and `s` is the signature proof.
+  #
+  # Parameters:
+  # * `msg` (`String`): A message string to sign.
+  # * `priv` (`BigInt`): A private key to sign with.
+  #
+  # ```
+  # sig = Secp256k1::Signature.sign "Hello, World!", BigInt.new("b795cd2c5ce0cc632ca1f65e921b9c751b363e97fcaeec81c02a85b763448268", 16)
+  # sig.r
+  # # => "63945398370917837063250848409972066837033757647691696776146735867163610886143"
+  # sig.s
+  # # => "20291418537568297129028959685291490143232574306335372594306006819765182564103"
+  # ```
   def self.sign(msg : String, priv : BigInt)
-    # calculate the message hash, using the cryptographic hash function sha-256
+    # Calculate the message hash, using the cryptographic hash function SHA-256.
     hash = BigInt.new Hash.sha256_string(msg), 16
 
-    # generate securely a random number k in the range [1..n-1]
-    # here: a new private key is the exact implementation of this requirement
+    # Securely generate a random number `k` in the range `[1..n-1]`;
+    # here: a new private key is the exact implementation of this requirement.
     k = Util.new_private_key
 
-    # calculate the random point r = k * g and take its x-coordinate: r = r.x
+    # Calculate the random point `r = k * g` and take its `x`-coordinate: `r = r.x`.
     r = Core.ec_mul(EC_BASE_G, k).x % EC_ORDER_N
 
-    # calculate the signature proof s = k^-1 * (h + r * priv) % n
+    # Calculate the signature proof `s = k^-1 * (h + r * priv) % n`.
     k_inv = Core.ec_mod_inv k, EC_ORDER_N
     s = ((hash + r * priv) * k_inv) % EC_ORDER_N
+
+    # Return the signature.
     sig = ECDSA_Signature.new r, s
     return sig
   end
 
-  # the algorithm to verify an ecdsa signature takes as input the signed message `msg`
-  # and the signature `(r, s)` produced from self.sign and the public key `pub`,
+  # Verifies a signature of a message against a public key.
+  #
+  # The algorithm to verify an ECDSA signature takes as input the signed message `msg`
+  # and the signature `(r, s)` produced from `sign` and the public key `pub`,
   # corresponding to the signer's private key. The result is boolean.
+  #
+  # Parameters:
+  # * `msg` (`String`): A message string to verify.
+  # * `sig` (`ECDSA_Signature`): A signature to verify the message.
+  # * `pub` (`EC_Point`): A public key to verify the signature against.
+  #
+  # ```
+  # pub = Secp256k1::Util.restore_public_key "03d885aed4bcaf3a8c95a57e3be08caa1bd6a060a68b9795c03129073597fcb19a"
+  # msg = "Hello, World!"
+  # sig = Secp256k1::ECDSA_Signature.new BigInt.new("63945398370917837063250848409972066837033757647691696776146735867163610886143"), BigInt.new("20291418537568297129028959685291490143232574306335372594306006819765182564103")
+  #
+  # Secp256k1::Signature.verify msg, sig, pub
+  # # => true
+  # ```
   def self.verify(msg : String, sig : ECDSA_Signature, pub : EC_Point)
-    # calculate the message hash, with the same hash function used during the signing
+    # Calculate the message hash, with the same hash function used during the signing.
     hash = BigInt.new Hash.sha256_string(msg), 16
     return verify_hash hash, sig, pub
   end
 
-  # same as self.verify, just using the hashed message directly
+  # Verifies a signature of a message hash against a public key.
+  #
+  # Same as `verify`, just using the hashed message directly.
+  #
+  # Parameters:
+  # * `hash` (`BigInt`): A SHA-256 hash of the message to verify.
+  # * `sig` (`ECDSA_Signature`): A signature to verify the message.
+  # * `pub` (`EC_Point`): A public key to verify the signature against.
+  #
+  # Returns _true_ if signature is valid. See `verify` for usage example.
   def self.verify_hash(hash : BigInt, sig : ECDSA_Signature, pub : EC_Point)
-    # calculate the modular inverse of the signature proof: s1 = s^{-1} % n
+    # Calculate the modular inverse of the signature proof: `s1 = s^{-1} % n`.
     s_inv = Core.ec_mod_inv sig.s, EC_ORDER_N
 
-    # recover the random point used during the signing: R' = (h * s1) * g + (r * s1) * pub
+    # Recover the random point used during the signing: `R' = (h * s1) * g + (r * s1) * pub`
     p0 = Core.ec_mul EC_BASE_G, (hash * s_inv) % EC_ORDER_N
     p1 = Core.ec_mul pub, (sig.r * s_inv) % EC_ORDER_N
     p = Core.ec_add p0, p1
 
-    # calculate the signature validation result by comparing whether r' == r
+    # Calculate the signature validation result by comparing whether `r' == r`.
     return sig.r === p.x
   end
 end
