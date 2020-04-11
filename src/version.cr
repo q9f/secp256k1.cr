e1a172fe922a09dc4955470d54e0472082d892d7
/raw PONG _GHMoaCXLT <58883403+q9f@users.noreply.github.com>
2020-04-11 22:12:22 +0200

    bump version to v0.3.2

--- src/version.cr
+++ src/version.cr
@@ -19,5 +19,5 @@
 # Ref: [en.bitcoin.it/wiki/Secp256k1](https://en.bitcoin.it/wiki/Secp256k1)
 module Secp256k1
   # The `VERSION` of the `Secp256k1` module.
-  VERSION = "0.3.1"
+  VERSION = "0.3.2"
 end
