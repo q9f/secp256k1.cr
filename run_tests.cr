b789b202c9954500696a9a12079286e2eedadbc8
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-09-29 13:05:47 +0200

    github: add codecov workflow

---
+++ run_tests.cr
@@ -0,0 +1,9 @@
+# wrapper to compile all tests into one binary for code coverage reporting
+require "../spec/bitcoin_spec.cr"
+require "../spec/core_spec.cr"
+require "../spec/ethereum_spec.cr"
+require "../spec/hash_spec.cr"
+require "../spec/secp256k1_spec.cr"
+require "../spec/signature_spec.cr"
+require "../spec/spec_helper.cr"
+require "../spec/util_spec.cr"
