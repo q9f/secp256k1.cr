fd8d05f63c958bdce302c9f94f90492e9a03fc16
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-09-29 14:44:19 +0200

    githubg: let linguist ignore html/js

---
+++ .github/coverage/index.js
@@ -0,0 +1,6 @@
+var data = {files:[
+{"link":"run_coverage.8cacf39ad4c932ca/index.html","title":"run_coverage","summary_name":"run_coverage","covered_class":"lineCov","covered":"97.1","covered_lines":"332","uncovered_lines":"10","total_lines" : "342"},
+], merged_files:[{"link":"kcov-merged/index.html","title":"[merged]","summary_name":"[merged]","covered_class":"lineCov","covered":"97.1","covered_lines":"332","uncovered_lines":"10","total_lines" : "342"},
+]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "run_coverage", "date" : "2021-09-29 14:42:48", "instrumented" : 342, "covered" : 332,};
---
+++ .github/coverage/run_coverage.8cacf39ad4c932ca/version.cr.36ce5c90.js
@@ -0,0 +1,28 @@
+var data = {lines:[
+{"lineNum":"    1","line":"# Copyright 2019-2020 @q9f","class":"lineCov","hits":"1","order":"26","possible_hits":"1",},
+{"lineNum":"    2","line":"#"},
+{"lineNum":"    3","line":"# Licensed under the Apache License, Version 2.0 (the \"License\");"},
+{"lineNum":"    4","line":"# you may not use this file except in compliance with the License."},
+{"lineNum":"    5","line":"# You may obtain a copy of the License at"},
+{"lineNum":"    6","line":"#"},
+{"lineNum":"    7","line":"#     http://www.apache.org/licenses/LICENSE-2.0"},
+{"lineNum":"    8","line":"#"},
+{"lineNum":"    9","line":"# Unless required by applicable law or agreed to in writing, software"},
+{"lineNum":"   10","line":"# distributed under the License is distributed on an \"AS IS\" BASIS,"},
+{"lineNum":"   11","line":"# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied."},
+{"lineNum":"   12","line":"# See the License for the specific language governing permissions and"},
+{"lineNum":"   13","line":"# limitations under the License."},
+{"lineNum":"   14","line":""},
+{"lineNum":"   15","line":"# Implements 256-bit `Secp256k1` Koblitz elliptic curve."},
+{"lineNum":"   16","line":"# Ref: [secg.org/sec2-v2.pdf](https://www.secg.org/sec2-v2.pdf)"},
+{"lineNum":"   17","line":"#"},
+{"lineNum":"   18","line":"# `Secp256k1` has the characteristic prime `p`, it is defined over the prime field ℤ_p."},
+{"lineNum":"   19","line":"# Ref: [en.bitcoin.it/wiki/Secp256k1](https://en.bitcoin.it/wiki/Secp256k1)"},
+{"lineNum":"   20","line":"module Secp256k1"},
+{"lineNum":"   21","line":"  # The `VERSION` of the `Secp256k1` module."},
+{"lineNum":"   22","line":"  VERSION = \"0.3.6\""},
+{"lineNum":"   23","line":"end"},
+]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "run_coverage", "date" : "2021-09-29 14:42:48", "instrumented" : 1, "covered" : 1,};
+var merged_data = [];
