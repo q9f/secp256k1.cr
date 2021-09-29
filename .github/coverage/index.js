fd024f565e69189f07083f075f9d59f9f9fe729f
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-09-29 14:34:59 +0200

    codecov: cleanup html/js

--- .github/coverage/index.js
+++
@@ -1,7 +0,0 @@
-var data = {files:[
-{"link":"run_tests.10fb90820ed1ba88/index.html","title":"run_tests","summary_name":"run_tests","covered_class":"lineCov","covered":"97.1","covered_lines":"332","uncovered_lines":"10","total_lines" : "342"},
-{"link":"run_coverage.8cacf39ad4c932ca/index.html","title":"run_coverage","summary_name":"run_coverage","covered_class":"lineCov","covered":"97.1","covered_lines":"332","uncovered_lines":"10","total_lines" : "342"},
-], merged_files:[{"link":"kcov-merged/index.html","title":"[merged]","summary_name":"[merged]","covered_class":"lineCov","covered":"97.1","covered_lines":"332","uncovered_lines":"10","total_lines" : "342"},
-]};
-var percent_low = 25;var percent_high = 75;
-var header = { "command" : "run_coverage", "date" : "2021-09-29 14:18:30", "instrumented" : 684, "covered" : 664,};
--- .github/run_coverage.sh
+++ .github/run_coverage.sh
@@ -2,3 +2,9 @@
 
 crystal build .github/run_coverage.cr -D skip-integrations -o .github/run_coverage
 kcov --clean --include-path=./src .github/coverage .github/run_coverage
+find .github/coverage -type f -name '*.js' -delete
+find .github/coverage -type f -name '*.htm' -delete
+find .github/coverage -type f -name '*.html' -delete
+find .github/coverage -type f -name '*.css' -delete
+find .github/coverage -type f -name '*.png' -delete
+find .github/coverage -type f -name '*.so' -delete
