8849970637f653d6e8f9a517dd1a08c9bc8bae30
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-12-13 12:05:53 +0100

    ci: bump codecov version (#53)

--- .github/run_coverage.sh
+++ .github/run_coverage.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+rm -r .github/coverage/
 crystal build .github/run_coverage.cr -D skip-integrations -o .github/run_coverage
 kcov --clean --include-path=./src .github/coverage .github/run_coverage
