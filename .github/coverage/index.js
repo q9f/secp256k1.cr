02905ab8c7d5649d5e2c477b23e6f42bec879b11
/raw PONG _GHMoaCXLT <58883403+q9f@users.noreply.github.com>
2021-12-13 12:04:16 +0100

    ci: bump codecov version

--- .github/coverage/index.js
+++ .github/coverage/index.js
@@ -1,6 +1,5 @@
 var data = {files:[
-{"link":"run_coverage.8cacf39ad4c932ca/index.html","title":"run_coverage","summary_name":"run_coverage","covered_class":"lineCov","covered":"97.1","covered_lines":"332","uncovered_lines":"10","total_lines" : "342"},
-], merged_files:[{"link":"kcov-merged/index.html","title":"[merged]","summary_name":"[merged]","covered_class":"lineCov","covered":"97.1","covered_lines":"332","uncovered_lines":"10","total_lines" : "342"},
-]};
+{"link":"run_coverage.5a209d3c/index.html","title":"run_coverage","summary_name":"run_coverage","covered_class":"lineCov","covered":"97.1","covered_lines":"331","uncovered_lines":"10","total_lines" : "341"},
+], merged_files:[]};
 var percent_low = 25;var percent_high = 75;
-var header = { "command" : "run_coverage", "date" : "2021-09-29 14:42:48", "instrumented" : 342, "covered" : 332,};
+var header = { "command" : "run_coverage", "date" : "2021-12-13 12:03:11", "instrumented" : 341, "covered" : 331,};
