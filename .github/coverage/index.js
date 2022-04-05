590636d0e2dc60ebc61f22ca486ab73b50147472
Afr Schoe <58883403+q9f@users.noreply.github.com>
2022-04-05 13:00:22 +0200

    kcov: run coverage

--- .github/coverage/index.js
+++ .github/coverage/index.js
@@ -1,5 +1,5 @@
 var data = {files:[
-{"link":"run_coverage.5a209d3c/index.html","title":"run_coverage","summary_name":"run_coverage","covered_class":"lineCov","covered":"97.1","covered_lines":"331","uncovered_lines":"10","total_lines" : "341"},
+{"link":"run_coverage.f831e6d8/index.html","title":"run_coverage","summary_name":"run_coverage","covered_class":"lineCov","covered":"95.9","covered_lines":"236","uncovered_lines":"10","total_lines" : "246"},
 ], merged_files:[]};
 var percent_low = 25;var percent_high = 75;
-var header = { "command" : "run_coverage", "date" : "2021-12-13 12:03:11", "instrumented" : 341, "covered" : 331,};
+var header = { "command" : "run_coverage", "date" : "2022-04-05 12:59:49", "instrumented" : 246, "covered" : 236,};
