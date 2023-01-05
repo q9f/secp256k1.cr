8a4f6f311d57b32d322bbd65f2e8fff2e8360383
Afr Schoe <58883403+q9f@users.noreply.github.com>
2023-01-05 17:18:04 +0100

    spec: happy new year

--- spec/secp256k1/key_spec.cr
+++ spec/secp256k1/key_spec.cr
@@ -1,4 +1,4 @@
-# Copyright 2019-2022 Afr Schoe @q9f
+# Copyright 2019-2023 Afri Schoedon @q9f
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
