6ab8bbcc7c3460c7cda02aa7633e83e2863dec83
Afr Schoe <58883403+q9f@users.noreply.github.com>
2023-01-05 17:17:34 +0100

    shard: bump version

--- src/secp256k1/version.cr
+++ src/secp256k1/version.cr
@@ -1,4 +1,4 @@
-# Copyright 2019-2022 Afr Schoe @q9f
+# Copyright 2019-2023 Afri Schoedon @q9f
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,5 +20,5 @@
 # Ref: [secg.org/sec2-v2.pdf](https://www.secg.org/sec2-v2.pdf)
 module Secp256k1
   # The `VERSION` of the `Secp256k1` module.
-  VERSION = "0.5.0"
+  VERSION = "0.5.1"
 end
